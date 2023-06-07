2a8b3c300f81d0aaf56abf6652ddbe3aad475ea1
Crisnineball <119613966+CrisHuynh@users.noreply.github.com>
2023-06-07 19:44:00 -0400

    Add files via upload

---
+++ w1p1/w1p1.c
@@ -0,0 +1,31 @@
+/*
+	****************************************************************************
+	< assessment name example : Workshop - #1 (Part - 1) >
+	Full Name : Xuan Khang Huynh
+	Student ID# : 105444228
+	Email : xhuynh@myseneca.ca
+	Section :
+	Authenticity Declaration :
+	I declare this submission is the result of my own work and has not been
+	shared with any other student or 3rd party content provider.This submitted
+	piece of work is entirely of my own creation.
+	* ****************************************************************************\n");
+
+*/
+
+
+#include <stdio.h>
+int main() {
+		printf("Workshop 1 Part-1\n");
+		printf("=================\n\n");
+		printf("I'm displaying this using the 'printf' stdio\n");
+		printf("(standard input output) library function!\n\n");
+		printf("Dear teacher,\n\n");
+		printf("  I promise I will work hard from this day onward.\n");
+		printf("  I acknowledge that practice is extremely important,\n");
+		printf("  so I will do all workshops, quizzes, and assignments.\n\n");
+		printf("Sincerely,\n\n");
+		printf("Xuan Khang Huynh\n");
+		printf("Student ID# 105444228\n");
+	return 0;
+}
